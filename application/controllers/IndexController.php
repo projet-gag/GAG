3d88f2f2a1a1061ae6708e351b2059ca8863b0d7
Bastien-Mirat <32570673+Bastien-Mirat@users.noreply.github.com>
2018-05-17 12:47:25 +0200

    Add files via upload

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../inc/connect-db.php';
+//extends Zend_Controller_Action
+class IndexController 
+{
+
+    public function init()
+    {
+
+    }
+
+    public function indexAction()
+    {  
+		
+    }
+
+
+}
+
